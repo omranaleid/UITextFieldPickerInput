679e92298642c8b8f1a2f1a40ad9cacd75961994
Omran Aleid <omran.aleid@bell.ca>
2020-06-05 12:36:08 -0400

    Add unit tests

--- UITextFieldPickerInputTests/UITextFieldPickerInputTests.swift
+++ UITextFieldPickerInputTests/UITextFieldPickerInputTests.swift
@@ -7,27 +7,73 @@
 //
 
 import XCTest
+@testable import UITextFieldPickerInput
+
+extension String: Selectable {
+    public var selectableText: String {
+        return self
+    }
+    
+    public var selectableValue: Any {
+        return self
+    }
+}
 
 class UITextFieldPickerInputTests: XCTestCase {
 
+    var textField: UITextField!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        textField = UITextField()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testDataInput() {
+        XCTAssertNil(textField.pickerInputView)
+        XCTAssertNil(textField.pickerInputView?.pickerData)
+        textField.loadPicker(data: [0: ["first", "second", "third"]])
+        XCTAssertNotNil(textField.pickerInputView)
+        XCTAssertNotNil(textField.pickerInputView?.pickerData)
     }
 
+    func testActions() {
+        XCTAssertNil(textField.pickerInputView?.onDone)
+        XCTAssertNil(textField.pickerInputView?.onSelect)
+        textField.loadPicker(data: [0: ["first", "second", "third"]], onSelect: { _ in
+            print("select")
+        }, onDone: { _ in
+            print("done")
+        })
+        XCTAssertNotNil(textField.pickerInputView?.onDone)
+        XCTAssertNotNil(textField.pickerInputView?.onSelect)
+    }
+    
+    func testUpdatePickerInputSettings() {
+        XCTAssertNil(textField.pickerInputView?.settings)
+        textField.loadPicker(data: [0: ["first", "second", "third"]])
+        textField.updatePickerInput(with: PickerInputSettings())
+        XCTAssertNotNil(textField.pickerInputView?.settings)
+    }
+    
+    func testUpdateData() {
+        let originalVal = [0: ["first", "second", "third"]]
+        let updatedVal = [0: ["first section"], 1: ["second section"]]
+        textField.loadPicker(data: originalVal)
+        XCTAssertNotNil(textField.pickerInputView?.pickerData)
+        XCTAssertTrue(textField.pickerInputView?.pickerData.keys.count == 1)
+        
+        textField.updatePickerInput(with: updatedVal)
+        XCTAssertNotNil(textField.pickerInputView?.pickerData)
+        XCTAssertTrue(textField.pickerInputView?.pickerData.keys.count == 2)
+    }
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
